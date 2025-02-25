0893cde86d10dc590e6abac8bcf4eefa91d2f580
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2025-02-25 09:59:33 -0600

    Se genero el primer caso de pruebas

--- haskell/Bowling/src/Bowling.hs
+++ haskell/Bowling/src/Bowling.hs
@@ -1,4 +1,4 @@
-module Bowling (someFunc) where
+module Bowling (someFunc, toFrames, score) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -7,8 +7,18 @@ data Frame = Open Int Int
             | Spare Int Int 
             | Strike Int Int 
 
+-- data Frame = Open { pins1 :: Int
+--                     , pins2 :: Int
+--                     }
+--                     | Spare { pins1 :: Int
+--                     , bonus1 :: Int
+--                     }
+--                     | Strike { bonus1 :: Int
+--                     , bonus2 :: Int
+--                     }
+
 toFrames :: [Int] -> [Frame]
 score :: [Frame] -> Int
 
-
-
+toFrames = undefined
+score = undefined
--- haskell/Bowling/test/Main.hs
+++ haskell/Bowling/test/Main.hs
@@ -1,4 +1,18 @@
 module Main (main) where
 
-main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+-- main :: IO ()
+-- main = putStrLn "Test suite not yet implemented."
+
+import Test.Tasty
+import Test.Tasty.HUnit
+import Bowling
+
+bowlingSuite :: TestTree
+bowlingSuite = testGroup "Bowling tests"
+                    [ testGroup "toFrames"
+                        [ testCase "zeros are zeros" $
+                            toFrames (replicate 20 0) @?= replicate 10 (Open 0 0)
+                        ]
+                    ]
+
+main = defaultMain bowlingSuite
