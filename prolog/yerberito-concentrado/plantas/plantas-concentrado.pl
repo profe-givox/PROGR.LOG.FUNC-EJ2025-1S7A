52ad2246bf204d3311dbf9a30645411c6a3ed553
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2025-05-07 09:40:52 -0600

    Se creo el archivo de concentrado de plantas

---
+++ prolog/yerberito-concentrado/plantas/plantas-concentrado.pl
@@ -0,0 +1,15 @@
+% Pagina 36.  abrojo
+nombre_cientifico(abrojo, tribulus_cistoides).
+contienente_origen(abrojo, america).
+pais_origen(abrojo, mexico).
+modo_preparacion(abrojo, cocimiento).
+enfermedad(infeccion_urinaria).
+enfermedad(cistitis).
+enfermedad(infeccion_riñon).
+sintoma_enfermedad(infeccion_urinaria, ardor_al_orinar).
+trata_enfermedad(abrojo, infeccion_urinaria).
+trata_enfermedad(abrojo, cistitis).
+accion_efecto_planta(abrojo, diuretico).
+accion_efecto_planta(abrojo, antiinflamatorio).
+modo_tratamiento(abrojo, tres_veces_dia).
+precaucion_planta(abrojo, embarazo).
