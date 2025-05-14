45cd29e6dc0fada3244738f60c1cafa1a6a38417
Arnold Reyes <166636441+ArnoldWiz@users.noreply.github.com>
2025-05-14 08:29:50 -0600

    Update plantas-concentrado.pl

--- prolog/yerberito-concentrado/plantas/plantas-concentrado.pl
+++ prolog/yerberito-concentrado/plantas/plantas-concentrado.pl
@@ -610,6 +610,65 @@ accion_efecto_planta(menta, refrescante).
 modo_tratamiento(menta, tomar_te_dos_tres_veces_dia).
 precaucion_planta(menta, reflujo_gastroesofagico).
 
+%Pagina 125 Oregano
+nombre_cientifico(oregano, origanum_vulgare).
+contienente_origen(oregano, europa).
+pais_origen(oregano, region_mediterranea).
+modo_preparacion(oregano, infusion).
+trata_enfermedad(oregano, problemas_digestivos).
+trata_enfermedad(oregano, resfriado).
+accion_efecto_planta(oregano, antiespasmodico).
+accion_efecto_planta(oregano, expectorante).
+modo_tratamiento(oregano, dos_veces_dia).
+precaucion_planta(oregano, evitar_dosis_altas).
+
+%Pagina 126 palo_de_flor
+nombre_cientifico(palo_de_flor, bourreria_huanita).
+contienente_origen(palo_de_flor, america).
+pais_origen(palo_de_flor, mexico).
+modo_preparacion(palo_de_flor, cocimiento).
+trata_enfermedad(palo_de_flor, fiebre).
+trata_enfermedad(palo_de_flor, dolor_de_cabeza).
+accion_efecto_planta(palo_de_flor, febrifugo).
+accion_efecto_planta(palo_de_flor, analgésico).
+modo_tratamiento(palo_de_flor, dos_veces_dia).
+precaucion_planta(palo_de_flor, no_prolongar_tratamiento).
+
+%Pagina 127 pasiflora
+nombre_cientifico(pasiflora, passiflora_incarnata).
+contienente_origen(pasiflora, america).
+pais_origen(pasiflora, mexico).
+modo_preparacion(pasiflora, infusion).
+trata_enfermedad(pasiflora, insomnio).
+trata_enfermedad(pasiflora, ansiedad).
+accion_efecto_planta(pasiflora, sedante).
+accion_efecto_planta(pasiflora, ansiolítico).
+modo_tratamiento(pasiflora, antes_de_dormir).
+precaucion_planta(pasiflora, no_combinar_con_alcohol).
+
+%Pagina 128 pericon
+nombre_cientifico(pericon, tagetes_lucida).
+contienente_origen(pericon, america).
+pais_origen(pericon, mexico).
+modo_preparacion(pericon, infusion).
+trata_enfermedad(pericon, colicos).
+trata_enfermedad(pericon, indigestión).
+accion_efecto_planta(pericon, digestivo).
+accion_efecto_planta(pericon, carminativo).
+modo_tratamiento(pericon, despues_comidas).
+precaucion_planta(pericon, evitar_embarazo).
+
+%Pagina 129 pinguica
+nombre_cientifico(pinguica, arctostaphylos_pungens).
+contienente_origen(pinguica, america).
+pais_origen(pinguica, mexico).
+modo_preparacion(pinguica, cocimiento).
+trata_enfermedad(pinguica, infeccion_urinaria).
+trata_enfermedad(pinguica, riñones).
+accion_efecto_planta(pinguica, diuretico).
+accion_efecto_planta(pinguica, depurativo).
+modo_tratamiento(pinguica, tres_veces_dia).
+precaucion_planta(pinguica, no_usar_por_periodos_largos).
 
 % Página 140 ruda
 nombre_cientifico(ruda, ruta_graveolens).
